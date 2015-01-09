10483ff03ce77f86e56a98eaa533154846bee1b4
zhengxin <zx19880528@qq.com>
2015-01-09 10:55:22 +0800

    add code auto layout

--- scrollViewDemo/CodeAutoLayoutViewController.m
+++ scrollViewDemo/CodeAutoLayoutViewController.m
@@ -24,13 +24,28 @@
         make.edges.equalTo(self.view);
     }];
     
+    UIImageView *vimImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"vim"]];
+    [scrollView addSubview:vimImageView];
+    [vimImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.and.top.equalTo(scrollView).offset(20);
+        make.right.equalTo(scrollView).offset(-20);
+    }];
+    
     UILabel *label = [UILabel new];
     label.numberOfLines = 0;
     label.text = @"label some label\n label some label";
     [scrollView addSubview:label];
-    
     [label mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.equalTo(scrollView).insets(UIEdgeInsetsMake(50, 50, 5, 5));
+        make.top.equalTo(vimImageView.mas_bottom).offset(8);
+        make.centerX.equalTo(vimImageView);
+    }];
+    
+    UIImageView *githubImageView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"github"]];
+    [scrollView addSubview:githubImageView];
+    [githubImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(label.mas_bottom).offset(8);
+        make.centerX.equalTo(vimImageView);
+        make.bottom.equalTo(scrollView).offset(-20);
     }];
 }
 
